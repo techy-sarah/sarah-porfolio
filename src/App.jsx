1ccf473066aaef1837f5ec3bba3d0ac060219667
Sarah Akan <abasiodiongsarah@gmail.com>
2024-06-05 11:00:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.scss"
+import Navbar from "./components/Navbar/Navbar";
+const App = () => {
+  return <div>
+    <section> <Navbar/> </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Porfolio 1</section>
+    <section>Porfolio 2</section>
+    <section>Porfolio 3</section>
+    <section>Contact Me</section>
+  </div>;
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">Navbar
+    {/* <Sidebar/> */}
+    
+
+    </div>
+  )
+}
+
+export default Navbar
