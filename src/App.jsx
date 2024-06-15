b8b96c63d675b2a4cee7afa697072bfb194d1ccd
Sarah Akan <abasiodiongsarah@gmail.com>
2024-06-15 01:29:10 +0100

    fixed build bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import "./app.scss";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import Parallax from "./components/parallax/Parallax";
 import Projects from "./components/work/Project";
+
 import Test from "./Test";
 const App = () => {
   return (
